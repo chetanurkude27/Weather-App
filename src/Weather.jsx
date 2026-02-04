6961b9f4fb4cce163698ab116edd5bebf3a61fb8
Chetan Urkude <chetanurkude27@gmail.com>
2026-02-04 18:46:26 +0530

    Update Weather.jsx

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -35,7 +35,7 @@ function Weather() {
       <input
         className="city"
         type='text'
-        placeholder='Enter City Name'
+        placeholder='Enter City Name:'
         value={city}
         onChange={handleCityChange}
       />
